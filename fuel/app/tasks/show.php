f1794d34715db2a7f2de0715b1349a8eb3ab88cb
kenjis <kenji.uui@gmail.com>
2015-12-06 15:52:42 +0900

    Fix output table format

--- fuel/app/tasks/show.php
+++ fuel/app/tasks/show.php
@@ -9,11 +9,11 @@ class Show
         $output = APPPATH . '/cache/benchmark-results.json';
         $results = json_decode(file_get_contents($output), true);
 
-        echo '|orm                |time (ms)|memory   |' . "\n";
-        echo '|-------------------|--------:|--------:|' . "\n";
+        echo '|orm                |time (ms)|memory (KB) |' . "\n";
+        echo '|-------------------|--------:|-----------:|' . "\n";
 
         foreach ($results as $orm => $result) {
-            printf("|%-19s|%9.2f|%9.2f|\n", $orm, round($result['time'], 2), round($result['memory'], 2));
+            printf("|%-19s|%9.2f|%12.2f|\n", $orm, round($result['time'], 2), round($result['memory'], 2));
         }
     }
 }
